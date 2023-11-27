6b36b58ea62d05d8cbd3c9a07339aac4b2cc7244
lam-ux <nononame460@gmail.com>
2023-11-27 21:03:15 +0700

    Little changes

--- ai/PBR/eval_norm.py
+++ ai/PBR/eval_norm.py
@@ -14,35 +14,36 @@ import matplotlib.pyplot as plt
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-#%%
+# %%
 PATH_CHK = "checkpoints/norm/norm_net_last.pth"
 CROP = 1024
 
-#%%
+# %%
 transform = transforms.Compose([
     transforms.Resize(CROP),
     transforms.CenterCrop(CROP),
     transforms.ToTensor(),
-    transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)) # (input - mean) / std
+    transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))  # (input - mean) / std
     # outputs range from -1 to 1
 ])
 
 transformDoNotResize = transforms.Compose([
-    #transforms.Resize(CROP),
-    #transforms.CenterCrop(CROP),
+    # transforms.Resize(CROP),
+    # transforms.CenterCrop(CROP),
     transforms.ToTensor(),
-    transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)) # (input - mean) / std
+    transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))  # (input - mean) / std
     # outputs range from -1 to 1
 ])
 
+
 class TestDataset(Dataset):
-    def __init__(self, img_dir, single = False):
-        if( single ):
+    def __init__(self, img_dir, single=False):
+        if (single):
             self.file_list = glob.glob(img_dir)
             self.names = [os.path.splitext(os.path.basename(fp))[0] for fp in self.file_list]
             return
 
-        self.file_list = glob.glob(img_dir+"/*.png")
+        self.file_list = glob.glob(img_dir + "/*.png")
         self.names = [os.path.splitext(os.path.basename(fp))[0] for fp in self.file_list]
 
     def __len__(self):
@@ -52,13 +53,14 @@ class TestDataset(Dataset):
         img = Image.open(self.file_list[i]).convert('RGB')
         h, w = img.size
 
-        if( w < 256 or h < 256 or w-300 > h or h-300 > w or w > 1024 or h > 1024 ):
+        if w < 256 or h < 256 or w - 300 > h or h - 300 > w or w > 1024 or h > 1024:
             img = transform(img)
         else:
             img = transformDoNotResize(img)
         return img, self.names[i]
 
-#%% test
+
+# %% test
 def generateNorm(net, DIR_FROM, DIR_EVAL):
     output_normal = DIR_EVAL
     if not os.path.exists(output_normal):
@@ -78,9 +80,10 @@ def generateNorm(net, DIR_FROM, DIR_EVAL):
             # print(img_name)
 
             img_out_filename = os.path.join(output_normal, f"{data[1][0]}_normal.png")
-            save_image(img_out, img_out_filename, value_range=(-1,1), normalize=True)
+            save_image(img_out, img_out_filename, value_range=(-1, 1), normalize=True)
 
     print("Done!")
+
 
 def generateNormSingle(net, DIR_FROM, DIR_EVAL):
     output_normal = DIR_EVAL
@@ -101,9 +104,10 @@ def generateNormSingle(net, DIR_FROM, DIR_EVAL):
             # print(img_name)
 
             img_out_filename = os.path.join(output_normal, f"{data[1][0]}_normal.png")
-            save_image(img_out, img_out_filename, value_range=(-1,1), normalize=True)
+            save_image(img_out, img_out_filename, value_range=(-1, 1), normalize=True)
 
     print("Done!")
+
 
 if __name__ == "__main__":
     from model import Unet
@@ -112,4 +116,4 @@ if __name__ == "__main__":
     checkpoint = torch.load(PATH_CHK)
     norm_net.load_state_dict(checkpoint["model"])
 
-    generateNorm(norm_net,"textures","out")
+    generateNorm(norm_net, "textures", "out")
